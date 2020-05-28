a279abb6ae960c054a9fddf708b08090096147af
Eric Koston Jr <EricKostonJr@gmail.com>
2020-05-28 12:33:24 -0400

    add static and dynamic test routes

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -7,6 +7,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config['DEBUG'] = True
+
 
 # use the decorator pattern to
 # link the view function to a url
@@ -15,7 +18,26 @@ app = Flask(__name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!?!?!?!?'
+
+# dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+# dynamic route that accepts slashes
+@app.route('/path/<path:value>')
+def path_type(value):
+    print(value)
+    return 'correct'
+
+@app.route('/name/<name>')
+def index(name):
+    if name.lower() == 'michael':
+        return 'Hello, {}'.format(name)
+    else:
+        return 'Not Found', 404
+
 
 # start the development server using the run() method
 if __name__ == '__main__':
