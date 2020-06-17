b857d6ddd6419004cd7e3015bec950474f3ad9e4
Eric Koston Jr <EricKostonJr@gmail.com>
2020-06-17 00:36:46 -0400

    add permissions

--- flasktaskr/project/test_tasks.py
+++ flasktaskr/project/test_tasks.py
@@ -6,7 +6,7 @@ import unittest
 
 from views import app, db
 from _config import basedir
-from models import User
+from models import Task, User
 
 
 TEST_DB = 'test.db'
@@ -80,19 +80,23 @@ class TaskTests(unittest.TestCase):
 		), follow_redirects=True)
 
 
+	def create_admin_user(self):
+		new_user = User(
+			name='Superman',
+			email='admin@realpython.com',
+			password='allpowerful',
+			role='admin'
+		)
+		db.session.add(new_user)
+		db.session.commit()
+
+
+
 	###############
 	#### tests ####
 	###############
 
 
-	# form is present page
-	def test_form_is_present(self):
-		response = self.app.get('/')
-		self.assertEqual(response.status_code, 200)
-		self.assertIn(b'Please login to access your task list.', response.data)
-
-
-	# users can access tasks
 	def test_logged_in_users_can_access_tasks_page(self):
 		self.register(
 			'Fletcher', 'fletcher@realpython.com', 'python101', 'python101'
@@ -107,7 +111,7 @@ class TaskTests(unittest.TestCase):
 		response = self.app.get('tasks/', follow_redirects=True)
 		self.assertIn(b'You need to login first.', response.data)
 
-	# users can add tasks (form validation)
+
 	def test_users_can_add_tasks(self):
 		self.create_user('Michael', 'michael@realpython.com', 'python')
 		self.login('Michael', 'python')
@@ -131,7 +135,6 @@ class TaskTests(unittest.TestCase):
 		self.assertIn(b'This field is required.', response.data)
 
 
-	# users can complete tasks
 	def test_users_can_complete_tasks(self):
 		self.create_user('Michael', 'michael@realpython.com', 'python')
 		self.login('Michael', 'python')
@@ -141,7 +144,6 @@ class TaskTests(unittest.TestCase):
 		self.assertIn(b'The task was marked as complete.', response.data)
 
 
-	# users can delete tasks
 	def test_users_can_delete_tasks(self):
 		self.create_user('Michael', 'michael@realpython.com', 'python')
 		self.login('Michael', 'python')
@@ -161,7 +163,80 @@ class TaskTests(unittest.TestCase):
 		self.login('Fletcher', 'python101')
 		self.app.get('tasks/', follow_redirects=True)
 		response = self.app.get('complete/1/', follow_redirects=True)
-		self.assertNotIn(b'The task was marked as complete.', response.data)
+		self.assertNotIn(
+			b'The task was marked as complete.', response.data
+		)
+		self.assertIn(
+			b'You can only update tasks that belong to you.', response.data
+		)
+
+
+	def test_users_cannot_delete_tasks_that_are_not_created_by_them(self):
+		self.create_user('Michael', 'michael@realpython.com', 'python')
+		self.login('Michael', 'python')
+		self.app.get('tasks/', follow_redirects=True)
+		self.create_task()
+		self.logout()
+		self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
+		self.login('Fletcher', 'python101')
+		self.app.get('tasks/', follow_redirects=True)
+		response = self.app.get("delete/1/", follow_redirects=True)
+		self.assertIn(
+			b'You can only delete tasks that belong to you.', response.data
+		)
+
+
+	def test_admin_users_can_complete_tasks_that_are_not_created_by_them(self):
+		self.create_user('Michael', 'michael@realpython.com', 'python')
+		self.login('Michael', 'python')
+		self.app.get('tasks/', follow_redirects=True)
+		self.create_task()
+		self.logout()
+		self.create_admin_user()
+		self.login('Superman', 'allpowerful')
+		self.app.get('tasks/', follow_redirects=True)
+		response = self.app.get("complete/1/", follow_redirects=True)
+		self.assertNotIn(
+			b'You can only update tasks that belong to you.', response.data
+		)
+
+
+	def test_admin_users_can_delete_tasks_that_are_not_created_by_them(self):
+		self.create_user('Michael', 'michael@realpython.com', 'python')
+		self.login('Michael', 'python')
+		self.app.get('tasks/', follow_redirects=True)
+		self.create_task()
+		self.logout()
+		self.create_admin_user()
+		self.login('Superman', 'allpowerful')
+		self.app.get('tasks/', follow_redirects=True)
+		response = self.app.get("delete/1/", follow_redirects=True)
+		self.assertNotIn(
+			b'You can only delete tasks that belong to you.', response.data
+		)
+
+
+	def test_string_representation_of_the_task_object(self):
+
+		from datetime import date
+		db.session.add(
+			Task(
+				"Run around in circles",
+				date(2015, 1, 22),
+				10,
+				date(2015, 1, 23),
+				1,
+				1
+			)
+		)
+
+		db.session.commit()
+
+		tasks = db.session.query(Task).all()
+		for task in tasks:
+			self.assertEqual(task.name, 'Run around in circles')
+
+
 
 
 if __name__ == "__main__":
